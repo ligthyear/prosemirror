47daad75a04d626d9a8e6bccdfc06ececabd00e0
Benjamin Kampmann <ben.kampmann@gmail.com>
2015-09-09 18:21:45 +0200

    first working version of replygif

--- demo/replygif.js
+++ demo/replygif.js
@@ -2,7 +2,7 @@ import {ProseMirror, defineOption, Keymap} from "../src/edit"
 import {nodeTypes, NodeType, Span} from "../src/model"
 import {render as renderDOM} from "../src/convert/to_dom"
 import {tags as parseTags} from "../src/convert/from_dom"
-import {registerItem, MenuItem} from "../src/menu/items"
+import {registerItem, MenuItem, IconItem} from "../src/menu/items"
 import {elt} from "../src/dom"
 import {addInputRules, Rule} from "../src/inputrules/inputrules"
 import {Tooltip} from "../src/menu/tooltip"
@@ -53,28 +53,96 @@ parseTags.img = (dom, context) => {
   else return oldImg(dom, context)
 }
 
-class DinoItem extends MenuItem {
-  constructor(image, action) {
+//     GIFS_URL = 
+
+class AjaxItems extends MenuItem {
+  constructor() {
     super()
-    this.image = image
-    this.action = action
   }
-  select(pm) { return pm.getOption("replygif") }
+  targetUrl(){ throw "You need to implement this"}
+  fetch(cb){
+    if (this.items && this.items.length) return cb();
+    this.loading = true;
+    ajaxGet(this.targetUrl(), function(sourceItems){
+        this.items = sourceItems.map(e=>this.createItem(e))
+        this.loading = false;
+        cb();
+      }.bind(this)
+    )
+  }
   render(menu) {
-    let button = elt("img", {src: "dino/" + this.image + ".png", class: "dinoicon", title: "Insert dinosaur"})
+    let button = this.button();
     button.addEventListener("mousedown", e => {
       e.preventDefault(); e.stopPropagation()
-      menu.run(this.action(menu.pm))
+      this.fetch(e=>{
+        menu.run(this.items)
+      })
     })
     return button
   }
 }
-let dinoItems = dinos.map(name => new DinoItem(name, pm => insertDino(pm, name)))
-registerItem("inline", new DinoItem("brontosaurus", () => dinoItems))
 
-var tagsUrl = "http://replygif.net/api/tags?api-key=39YAprx5Yi&reactions=1";
+class GifItem extends MenuItem {
+  constructor(gif) {
+    super()
+    this.image = gif.file;
+    this.thumbnail = gif.file.replace('/i/', "/thumbnail/");
+    this.title = gif["caption"];
+  }
+  render(menu) {
+    let button = elt("img", {src: this.thumbnail, title: this.title})
+    let over = button.addEventListener("mouseover", e => {
+      button.src = this.image;
+    })
+    button.addEventListener("mousedown", e => {
+      e.preventDefault(); e.stopPropagation()
+      menu.run(this.insert(menu.pm))
+    })
+    return button
+  }
+  insert(pm, name) {
+    pm.apply(pm.tr.insertInline(pm.selection.head,
+              new Span("image", {src: this.image,
+                                 alt: this.title,
+                                 title: this.title}, null, null)))
+  }
+}
 
-ajaxGet(tagsUrl, (e)=>console.log(e), (e)=>console.log(e));
+
+class TagItem extends AjaxItems {
+  constructor(itemCtx){
+    super()
+    this.tag = itemCtx.title;
+  }
+  createItem(e){
+    return new GifItem(e);
+  }
+  targetUrl(){
+    return "http://replygif.net/api/gifs?api-key=39YAprx5Yi&tag=" + this.tag;
+  }
+  button() {
+    return elt("span", {}, this.tag);
+  }
+}
+
+class ReplyGifItem extends AjaxItems {
+  createItem(e){
+    return new TagItem(e);
+  }
+  constructor() {
+    super()
+    this.tags = false;
+  }
+  targetUrl(){
+    return "http://replygif.net/api/tags?api-key=39YAprx5Yi&reaction=1";
+  }
+  select(pm) { return pm.getOption("replygif") }
+  button() {
+    return elt("span", {}, "ReplyGif");
+  }
+}
+// let dinoItems = dinos.map(name => new DinoItem(name, pm => insertDino(pm, name)))
+registerItem("inline", new ReplyGifItem())
 
 function insertDino(pm, name) {
   pm.apply(pm.tr.insertInline(pm.selection.head, new Span("dino", {type: name})))
